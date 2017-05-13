2c6556341671beead52f9701afd423fef0856def
wllm-git <wllm.gtr@gmail.com>
2017-05-13 10:01:49 -0300

    criação do projeto.

---
+++ app/src/main/java/projetopgm/com/br/projetopgm/MainActivityFragment.java
@@ -0,0 +1,19 @@
+package projetopgm.com.br.projetopgm;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
