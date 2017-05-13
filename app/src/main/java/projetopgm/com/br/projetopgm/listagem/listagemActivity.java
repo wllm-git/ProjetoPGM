24991768e6524da4494338a18124bd0270ffc7cb
danirsilva <danielrsilva93@gmail.com>
2017-05-13 18:22:14 -0300

    criando tela de listagem e detalhes activity

---
+++ app/src/main/java/projetopgm/com/br/projetopgm/listagem/listagemActivity.java
@@ -0,0 +1,15 @@
+package projetopgm.com.br.projetopgm.listagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import projetopgm.com.br.projetopgm.R;
+
+public class listagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listagem);
+    }
+}
