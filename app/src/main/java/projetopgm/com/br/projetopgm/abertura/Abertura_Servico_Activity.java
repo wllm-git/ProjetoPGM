185b272e7c6cf2eb2d5ef8669dcd521729bcf641
roldaowilker <roldao.wilker@gmail.com>
2017-05-13 20:31:11 -0300

    Criação dos pacotes: Acompanhamento e Abertura de serviços. Criação da Activity Abertura_Serviço

---
+++ app/src/main/java/projetopgm/com/br/projetopgm/abertura/Abertura_Servico_Activity.java
@@ -0,0 +1,15 @@
+package projetopgm.com.br.projetopgm.abertura;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import projetopgm.com.br.projetopgm.R;
+
+public class Abertura_Servico_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abertura_servico);
+    }
+}
