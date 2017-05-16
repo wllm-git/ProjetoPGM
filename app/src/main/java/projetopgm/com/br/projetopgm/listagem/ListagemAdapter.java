62e80d446eba0b02f0f94a8c9c3fa5544b707f7f
danirsilva <danielrsilva93@gmail.com>
2017-05-16 20:58:52 -0300

    finalizando Adapter de servicos fechados, criação de fragments, criação de ListView

--- app/src/main/java/projetopgm/com/br/projetopgm/listagem/ListagemAdapter.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/listagem/ListagemAdapter.java
@@ -1,28 +1,60 @@
 package projetopgm.com.br.projetopgm.listagem;
 
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+import projetopgm.com.br.projetopgm.R;
+import projetopgm.com.br.projetopgm.base.Servico;
 
 public class ListagemAdapter extends BaseAdapter {
+
+    private ArrayList<Servico> servicos;
+    private Context context;
+
+    public ListagemAdapter(Context context, ArrayList<Servico> servicos) {
+        this.servicos = servicos;
+        this.context = context;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return servicos.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+
+        return servicos.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        convertView = LayoutInflater.from(context).inflate(R.layout.fragment_listagem_item, null);
+
+        Servico s = servicos.get(position);
+        //ImageView img = (ImageView) convertView.findViewById(R.id.imgIcone);
+        TextView dsc = (TextView) convertView.findViewById(R.id.tvwDesc);
+        TextView dt = (TextView) convertView.findViewById(R.id.tvwData);
+
+        dsc.setText(s.getDescricao());
+        dt.setText(s.getDataFechamento().toString());
+
+        return convertView;
     }
 }
