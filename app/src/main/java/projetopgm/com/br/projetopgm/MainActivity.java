fafb015559b95373fa3cd92138fbf34227ef4df2
wllm-git <wllm.gtr@gmail.com>
2017-05-17 01:31:06 -0300

    implementado navegação para a tela de criação. correção dos tipos de layout para RelativeLayout. correção do arquivo activity_listagem para carregar o fragment corretamente. atualização do AndroidManifest com todas as activities.

--- app/src/main/java/projetopgm/com/br/projetopgm/MainActivity.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/MainActivity.java
@@ -3,13 +3,13 @@ package projetopgm.com.br.projetopgm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import projetopgm.com.br.projetopgm.abertura.AberturaServicoAcivity;
 import projetopgm.com.br.projetopgm.listagem.ListagemActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Teste", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent it = new Intent(MainActivity.this, AberturaServicoAcivity.class);
+                startActivity(it);
             }
         });
     }
--- app/src/main/java/projetopgm/com/br/projetopgm/detalhes/DetalhesActivity.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/detalhes/DetalhesActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import projetopgm.com.br.projetopgm.R;
 
-public class detalhesActivity extends AppCompatActivity {
+public class DetalhesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
