467bcfed766c43921b9d3a29a086ddf7575e29cd
roldaowilker <roldao.wilker@gmail.com>
2017-05-20 12:25:01 -0300

    Inclusão do fragment abertura_servico_detalhes na Activity AberturaServico.

--- app/src/main/java/projetopgm/com/br/projetopgm/abertura/AberturaServicoAcivity.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/abertura/AberturaServicoAcivity.java
@@ -1,12 +1,18 @@
 package projetopgm.com.br.projetopgm.abertura;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import projetopgm.com.br.projetopgm.R;
+import projetopgm.com.br.projetopgm.base.Servico;
 
 public class AberturaServicoAcivity extends AppCompatActivity {
+
+    public static final String EXTRA_SERVICO_ABERTURA = "abertura";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,17 @@ public class AberturaServicoAcivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
+
+
+        Intent intent = getIntent();
+        Servico servico = (Servico) intent.getSerializableExtra(EXTRA_SERVICO_ABERTURA);
+
+        AberturaFrament frament = AberturaFrament.novaInstancia(servico);
+        FragmentManager fm = getSupportFragmentManager();
+
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.replace(R.id.aberturaDetalhes, frament,AberturaFrament.TAG_DETALHE);
+        ft.commit();
     }
 }
