1933e2cb1005be111a4bb582289ce93a3a225e48
wllm-git <wllm.gtr@gmail.com>
2017-05-24 01:46:51 -0300

    Alteração na forma de buscar a localização atual e alteração na forma de atualização das rotas.

--- app/src/main/java/projetopgm/com/br/projetopgm/localizacao/LocalizacaoHelper.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/localizacao/LocalizacaoHelper.java
@@ -1,7 +1,6 @@
 package projetopgm.com.br.projetopgm.localizacao;
 
 import android.content.Context;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,20 +8,35 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 
 public class LocalizacaoHelper implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener{
+    private static long LOCATION_INTERVAL = 2000;
 
     private Context context;
     private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private FusedLocationProviderApi fusedLocationProviderApi;
+    private LocationListener locationListener;
     private LatLng local;
 
-    public LocalizacaoHelper(Context context, GoogleApiClient.ConnectionCallbacks callback) {
+    public LocalizacaoHelper(Context context, LocationListener locationListener) {
         this.context = context;
+        this.locationListener = locationListener;
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(LOCATION_INTERVAL);
+        locationRequest.setFastestInterval(LOCATION_INTERVAL);
+
+        fusedLocationProviderApi = LocationServices.FusedLocationApi;
+
         googleApiClient = new GoogleApiClient.Builder(context)
-                .addConnectionCallbacks(callback)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
@@ -41,7 +55,11 @@ public class LocalizacaoHelper implements GoogleApiClient.ConnectionCallbacks,
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) { }
+    public void onConnected(@Nullable Bundle bundle) {
+        try{
+            fusedLocationProviderApi.requestLocationUpdates(googleApiClient, locationRequest, locationListener);
+        }catch (SecurityException ex){ }
+    }
 
     @Override
     public void onConnectionSuspended(int i) {
@@ -52,7 +70,7 @@ public class LocalizacaoHelper implements GoogleApiClient.ConnectionCallbacks,
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Toast.makeText(context, connectionResult.getErrorCode() + " - " + connectionResult.getErrorMessage(), Toast.LENGTH_LONG);
     }
-
+    /*
     public LatLng obterLocalizacao(){
         try{
             Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
@@ -64,5 +82,5 @@ public class LocalizacaoHelper implements GoogleApiClient.ConnectionCallbacks,
             Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG);
         }
         return local;
-    }
+    }*/
 }
--- app/src/main/java/projetopgm/com/br/projetopgm/localizacao/MapsActivity.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/localizacao/MapsActivity.java
@@ -1,18 +1,19 @@
 package projetopgm.com.br.projetopgm.localizacao;
 
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
@@ -21,54 +22,14 @@ import java.util.List;
 import projetopgm.com.br.projetopgm.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks{
+        LocationListener {
 
     private GoogleMap mMap;
     private LocalizacaoHelper localizacaoHelper;
     private LoaderManager loaderManager;
     private LatLng oficina;
     private LatLng local;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-
-        localizacaoHelper = new LocalizacaoHelper(this, this);
-        loaderManager = getSupportLoaderManager();
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        oficina = new LatLng(-8.1280737,-34.9154712);
-        mMap.addMarker(new MarkerOptions().position(oficina).title("Oficina"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(oficina, 14.5f));
-    }
-
-    public void atualizarMap(){
-        mMap.addMarker(new MarkerOptions().position(local));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(local, 14.5f));
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(localizacaoHelper != null)
-            localizacaoHelper.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        if(localizacaoHelper != null)
-            localizacaoHelper.onStop();
-        super.onStop();
-    }
-
+    private Marker marker;
     LoaderManager.LoaderCallbacks<List<LatLng>> rotaCallbacks = new LoaderManager.LoaderCallbacks<List<LatLng>>() {
         @Override
         public Loader<List<LatLng>> onCreateLoader(int id, Bundle args) {
@@ -94,12 +55,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     };
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        local = localizacaoHelper.obterLocalizacao();
-        if(local != null)
-            loaderManager.initLoader(12, null, rotaCallbacks);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        localizacaoHelper = new LocalizacaoHelper(this, this);
+        loaderManager = getSupportLoaderManager();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onConnectionSuspended(int i) { }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        oficina = new LatLng(-8.1280737,-34.9154712);
+        mMap.addMarker(new MarkerOptions().position(oficina).title("Oficina"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(oficina, 14.5f));
+    }
+
+    public void atualizarMap(){
+        if(marker != null)
+            marker.setPosition(local);
+        else
+            marker = mMap.addMarker(new MarkerOptions().position(local));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(local));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(localizacaoHelper != null)
+            localizacaoHelper.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        if(localizacaoHelper != null)
+            localizacaoHelper.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location != null) {
+            local = new LatLng(location.getLatitude(), location.getLongitude());
+            loaderManager.initLoader(12, null, rotaCallbacks);
+        }
+    }
 }
