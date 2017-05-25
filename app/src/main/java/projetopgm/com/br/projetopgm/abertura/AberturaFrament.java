eb808417bd71a19a7bc9cac38b5ae858777db48d
roldaowilker <roldao.wilker@gmail.com>
2017-05-25 10:20:04 -0300

    Modificações Framents

--- app/src/main/java/projetopgm/com/br/projetopgm/abertura/AberturaFrament.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/abertura/AberturaFrament.java
@@ -1,8 +1,23 @@
 package projetopgm.com.br.projetopgm.abertura;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import projetopgm.com.br.projetopgm.R;
+
 /**
  * Created by Roldao Wilker on 19/05/2017.
  */
 
-public class AberturaFrament {
+public class AberturaFrament extends Fragment{
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_abertura_detalhes, container, false);
+    }
 }
--- app/src/main/java/projetopgm/com/br/projetopgm/acompanhamento/AcompanhamentoFragment.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/acompanhamento/AcompanhamentoFragment.java
@@ -1,8 +1,26 @@
 package projetopgm.com.br.projetopgm.acompanhamento;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import projetopgm.com.br.projetopgm.R;
+
 /**
  * Created by Roldao Wilker on 19/05/2017.
  */
 
-public class AcompanhamentoFragment {
+public class AcompanhamentoFragment extends Fragment{
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_acompanhamento_detalhes, container, false);
+    }
+
+
 }
