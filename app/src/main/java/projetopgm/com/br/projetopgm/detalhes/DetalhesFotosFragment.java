7f2ec6fdc879d07ac895a6f0cbe395f7cc6ef835
wllm-git <wllm.gtr@gmail.com>
2017-06-04 12:50:25 -0300

    Implementação da lógica das fotos no DetalhesFotosFragment.

--- app/src/main/java/projetopgm/com/br/projetopgm/detalhes/DetalhesFotosFragment.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/detalhes/DetalhesFotosFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import projetopgm.com.br.projetopgm.R;
@@ -15,6 +16,7 @@ import projetopgm.com.br.projetopgm.compartilhados.FuncoesGlobais;
 
 public class DetalhesFotosFragment extends Fragment {
 
+    private Servico servico;
 
     public DetalhesFotosFragment() {
 
@@ -32,16 +34,36 @@ public class DetalhesFotosFragment extends Fragment {
         }
 
         Intent it = getActivity().getIntent();
-        Servico servico = (Servico) it.getSerializableExtra("servico");
-        if (servico != null){
-          // TODO LEMBRAR DE FAZER A LISTA DE FOTOS DENTRO DO FRAGMENT DETALHES FOTOS
-
-
-        }
-
+        servico = (Servico) it.getSerializableExtra("servico");
+        if (servico != null)
+            atualizarFotos(fragmentFotos);
 
         return fragmentFotos;
-
     }
 
+    private void atualizarFotos(View viewGroup){
+
+        switch (servico.getFotos().size()){
+            case 6:
+                atualizarImageView(R.id.imagem6, viewGroup);
+            case 5:
+                atualizarImageView(R.id.imagem5, viewGroup);
+            case 4:
+                atualizarImageView(R.id.imagem4, viewGroup);
+            case 3:
+                atualizarImageView(R.id.imagem3, viewGroup);
+            case 2:
+                atualizarImageView(R.id.imagem2, viewGroup);
+            case 1:
+                atualizarImageView(R.id.imagem1, viewGroup);
+        }
+    }
+
+    private void atualizarImageView(int id, View viewGroup){
+        ImageView imageView = (ImageView) viewGroup.findViewById(id);
+        int x = imageView.getDrawable().getMinimumWidth();
+        int y = imageView.getDrawable().getMinimumHeight();
+        String path = servico.getFotos().get(0).getArquivo();
+        imageView.setImageBitmap(FuncoesGlobais.decodeFile(path, 100, 100));
+    }
 }
