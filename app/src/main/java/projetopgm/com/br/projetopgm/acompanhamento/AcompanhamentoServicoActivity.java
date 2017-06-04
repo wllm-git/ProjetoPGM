e67cb46c013190cd0702eefe0b83091c0f653761
wllm-git <wllm.gtr@gmail.com>
2017-06-04 14:30:47 -0300

    Implementação de confirmação de teste.

--- app/src/main/java/projetopgm/com/br/projetopgm/acompanhamento/AcompanhamentoServicoActivity.java
+++ app/src/main/java/projetopgm/com/br/projetopgm/acompanhamento/AcompanhamentoServicoActivity.java
@@ -1,12 +1,20 @@
 package projetopgm.com.br.projetopgm.acompanhamento;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import projetopgm.com.br.projetopgm.R;
+import projetopgm.com.br.projetopgm.bancodados.ServicoDAO;
+import projetopgm.com.br.projetopgm.base.Servico;
+import projetopgm.com.br.projetopgm.webservice.ServicoWebTask;
 
-public class AcompanhamentoServicoActivity extends AppCompatActivity {
+public class AcompanhamentoServicoActivity extends AppCompatActivity
+        implements View.OnClickListener{
+    private Servico servico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,23 @@ public class AcompanhamentoServicoActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAceitar);
+        fab.setOnClickListener(this);
+
+        Intent it = getIntent();
+        servico = (Servico) it.getSerializableExtra("servico");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(servico != null && servico.getTipo()== Servico.Tipo.ORCAMENTO
+                && servico.getStatus()== Servico.Status.ANDAMENTO){
+            ServicoDAO servicoDAO = new ServicoDAO(this);
+            servico.setTipo(Servico.Tipo.OS);
+            servicoDAO.salvar(servico);
+            new ServicoWebTask().execute(servico);
+            finish();
+        }
     }
 }
